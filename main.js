9e2c856760c50563d856bea6d74c2cce3d4964f5
Ridyl <joey.dalatri214@gmail.com>
2024-12-10 14:26:44 -0500

    added all document selectors to JS file

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+// User Inputs
+let amountDue = document.getElementById("amount-due");
+let amountRec = document.getElementById("amount-received");
+
+// Calc Button
+let calcButton = document.getElementById("calculate-change");
+
+// Output Areas
+let dollarOut = document.getElementById("dollars-output");
+let quarterOut = document.getElementById("quarters-output");
+let dimeOut = document.getElementById("dimes-output");
+let nickelOut = document.getElementById("nickels-output");
+let pennyOut = document.getElementById("pennies-output");
 
